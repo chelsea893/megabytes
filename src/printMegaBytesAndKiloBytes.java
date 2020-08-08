bbfa90a420e9e26e3a214ab91c67f6548116e85f
Chelsea Chen <chelseachen893@gmail.com>
2020-08-07 18:28:26 -0600

    Initial commit

---
+++ src/printMegaBytesAndKiloBytes.java
@@ -0,0 +1,15 @@
+public class MegaBytesAndKiloBytes {
+
+    public static void printMegaBytesAndKiloBytes() {
+        int kiloBytes = 2500;
+        if (kiloBytes > 0) {
+            int YY = kiloBytes / 1024;
+            int ZZ = kiloBytes % 1024;
+            System.out.println(kiloBytes + "KB=" + YY + "MB and " + ZZ + "KB");
+        }else{
+            System.out.println("invalid value");
+        }
+
+
+    }
+}
